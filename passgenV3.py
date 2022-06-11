fba0571f3ab067ae2eca21470f2a0e727be79b2a
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2022-06-11 12:47:18 +0100

    Update passgenV3.py

--- passgenV3.py
+++ passgenV3.py
@@ -3,16 +3,23 @@
 import random
 from time import sleep
 
-char="abcdefjhigklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/*-.!%:;,.?µ=+}])°[({'#~&"
+char="abcdefjhigklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/*-.!%:;,.?µ=+}])°[({'#~&0123456789"
+def banner():
+    print("@AllRightReserved to cybereagle2001")
+    print("\033[1;34m  _ __   __ _ ___ ___  __ _  ___ _ __")
+    print("|  _ \ / _  / __/ __|/ _  |/ _ \  _ \"")
+    print("| |_) | (_| \__ \__ \ (_| |  __/ | | |")
+    print("| .__/ \__,_|___/___/\__, |\___|_| |_|")
+    print("|_|                  |___/cybereagle2001 ")
+    print("\033[1;37m")
 
-print("@AllRightReserved to cybereagle2001")
-print("\033[1;34m  _ __   __ _ ___ ___  __ _  ___ _ __")
-print("|  _ \ / _  / __/ __|/ _  |/ _ \  _ \"")
-print("| |_) | (_| \__ \__ \ (_| |  __/ | | |")
-print("| .__/ \__,_|___/___/\__, |\___|_| |_|")
-print("|_|                  |___/cybereagle2001 ")
-print("\033[1;37m")
+banner()
 n=int(input("write the length of your password: "))
+if (n < 8):
+    os.system("clear")
+    banner()
+    print("the minimum length for a secure password is 8 caracters!!\n")
+    n= int(input("write the length of your password:"))
 for c in range(100):
     password=''
     for loop in range(n):
